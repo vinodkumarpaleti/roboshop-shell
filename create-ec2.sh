5a427fd10513318508096c365a4fcb6b17d8213f
vinodkumar <kumarvinod.paleti@gmail.com>
2023-11-07 23:13:28 +0530

    create-ec2.sh file added

--- create-ec2.sh
+++ create-ec2.sh
@@ -3,7 +3,7 @@
 NAMES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "web")
 INSTANCE_TYPE=""
 IMAGE_ID=ami-03265a0778a880afb
-SECURITY_GROUP_ID=sg-0189d4870cc65fd55
+SECURITY_GROUP_ID=sg-079bfb9b54023a1c5
 DOMAIN_NAME=jcglobalit.online
 
 # if mysql or mongodb instance_type should be t3.medium , for all others it is t2.micro
@@ -19,7 +19,9 @@ do
     echo "creating $i instance"
     IP_ADDRESS=$(aws ec2 run-instances --image-id $IMAGE_ID  --instance-type $INSTANCE_TYPE --security-group-ids $SECURITY_GROUP_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" | jq -r '.Instances[0].PrivateIpAddress')
     echo "created $i instance: $IP_ADDRESS"
+    #ec2 run-instances --image-id ami-03265a0778a880afb --instance-type t2.micro --security-group-ids sg-079bfb9b54023a1c5
 
+aws 
     aws route53 change-resource-record-sets --hosted-zone-id Z02954502A6LVQUVQ4MZL --change-batch '
     {
             "Changes": [{
